61660f794137c8feeac7de74aa700ef76e0f1ab6
Gyunam Park <parkguenam@gmail.com>
2023-11-29 14:14:07 +0900

    upload

---
+++ main.py
@@ -0,0 +1,9 @@
+from data_preprocessing import data_preprocessing
+from project_update_db import project_update_db
+
+# Use this to make table data in csv format
+data_preprocessing()
+
+# Use this tomake database and put table in it
+project_update_db()
+
--- project_update_db.py
+++ project_update_db.py
@@ -1,82 +1,145 @@
-import csv
-import mysql.connector
+def project_update_db():
+    import csv
+    import mysql.connector
+    from datetime import datetime
 
-# Use this code only first you create your database
-# mydb = mysql.connector.connect(
-#     host = "localhost",
-#     user= "root",
-#     passwd = "8812"  
-# )
-    
-# mycursor = mydb.cursor()
 
-# mycursor.execute("CREATE DATABASE db_project")
-
-def connectDB(db_use):
+    # Use this code only first you create your database
     mydb = mysql.connector.connect(
         host = "localhost",
         user= "root",
-        passwd = "8812",
-        database = db_use
+        passwd = "5789"  
     )
+    mycursor = mydb.cursor()
+    mycursor.execute("CREATE DATABASE IF NOT EXISTS db_project")
+
+    def connectDB(db_use):
+        mydb = mysql.connector.connect(
+            host = "localhost",
+            user= "root",
+            passwd = "5789",
+            database = db_use
+        )
+        mycursor = mydb.cursor(prepared=True)
+        return mydb, mycursor
+
+    mydb, myCursor = connectDB("db_project")
+
+
+    # movies table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS movies(movie_id INT PRIMARY KEY, title VARCHAR(255), last_update DATE, release_year SMALLINT, rating VARCHAR(255), duration SMALLINT, description TEXT)")
+    with open('./for_use/movies.csv', encoding='UTF-8') as csvfile:
+        movies_data = csv.reader(csvfile)
+        header = next(movies_data)  # Skip header
+        for data in movies_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO movies(movie_id, title, last_update, release_year, rating, duration, description) VALUES (%s, %s, %s, %s, %s, %s, %s)", (int(data[1]), data[2], last_update, int(data[4]), data[5], int(data[6]), data[7]))
+    mydb.commit()
+
+
+    # ott_service table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS ott_service (movie_id INT PRIMARY KEY, ott_service VARCHAR(255), last_update DATE)")
+
+    with open('./for_use/ott_service.csv', encoding='UTF-8') as csvfile:
+        ott_data = csv.reader(csvfile)
+        header = next(ott_data)  # Skip header
+        for data in ott_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO ott_service (movie_id, ott_service, last_update) VALUES (%s, %s, %s)", (int(data[1]), data[2], last_update))
+    mydb.commit()
+
+
+    # direct table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS direct (movie_id INT, director_id INT, last_update DATE, PRIMARY KEY (movie_id, director_id))")
+
+    with open('./for_use/direct.csv', encoding='UTF-8') as csvfile:
+        director_data = csv.reader(csvfile)
+        header = next(director_data)  # Skip header
+        for data in director_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO direct (movie_id, director_id, last_update) VALUES (%s, %s, %s)", (int(data[1]), int(data[2]), last_update))
+    mydb.commit()
+
+
+    # director table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS director (director_id INT PRIMARY KEY, director_name VARCHAR(255), last_update DATE)")
+
+    with open('./for_use/director.csv', encoding='UTF-8') as csvfile:
+        director_data = csv.reader(csvfile)
+        header = next(director_data)  # Skip header
+        for data in director_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO director (director_id, director_name, last_update) VALUES (%s, %s, %s)", (int(data[1]), data[2], last_update))
+    mydb.commit()
+
+
+    # cast table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS cast (movie_id INT, actor_id INT, last_update DATE, PRIMARY KEY (movie_id, actor_id))")
+
+    with open('./for_use/cast.csv', encoding='UTF-8') as csvfile:
+        cast_data = csv.reader(csvfile)
+        header = next(cast_data)  # Skip header
+        for data in cast_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO cast (movie_id, actor_id, last_update) VALUES (%s, %s, %s)", (int(data[1]), int(data[2]), last_update))
+    mydb.commit()
+        
+        
+    # actor table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS actor (actor_id INT PRIMARY KEY, actor_name VARCHAR(255), last_update DATE)")
+
+    with open('./for_use/actor.csv', encoding='UTF-8') as csvfile:
+        actor_data = csv.reader(csvfile)
+        header = next(actor_data)  # Skip header
+        for data in actor_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO actor (actor_id, actor_name, last_update) VALUES (%s, %s, %s)", (int(data[1]), data[2], last_update))
+    mydb.commit()
+        
+        
+    # movies_genre table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS movies_genre (movie_id INT, genre_id INT, last_update DATE, PRIMARY KEY (movie_id, genre_id))")
+
+    with open('./for_use/movies_genre.csv', encoding='UTF-8') as csvfile:
+        movies_genre_data = csv.reader(csvfile)
+        header = next(movies_genre_data)  # Skip header
+        for data in movies_genre_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO movies_genre (movie_id, genre_id, last_update) VALUES (%s, %s, %s)", (int(data[1]), int(data[2]), last_update))
+    mydb.commit()
     
-    mycursor = mydb.cursor(prepared=True)
-    return mydb, mycursor
+    
+    # genre table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS genre (genre_id INT PRIMARY KEY, genre_name VARCHAR(255), last_update DATE)")
 
-mydb, myCursor = connectDB("db_project")
-
-
-# movies table
-csv_data = csv.reader(open('./data/movies.csv', encoding='UTF-8'))
-header = next(csv_data)
-
-myCursor.execute("CREATE TABLE movies(movie_id INT PRIMARY KEY, title VARCHAR(255), director VARCHAR(4000), date_added VARCHAR(255), release_year INT, rating VARCHAR(255), duration VARCHAR(255))")
-for data in csv_data:
-    data = data[1:]
-    myCursor.execute("INSERT INTO movies (movie_id, title, director, date_added, release_year, rating, duration) VALUES (%s, %s, %s, %s, %s, %s, %s)", data)
-mydb.commit()
+    with open('./for_use/genre.csv', encoding='UTF-8') as csvfile:
+        genre_data = csv.reader(csvfile)
+        header = next(genre_data)  # Skip header
+        for data in genre_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO genre (genre_id, genre_name, last_update) VALUES (%s, %s, %s)", (int(data[1]), data[2], last_update))
+    mydb.commit()
 
 
-# ott_service table
-ott_data = csv.reader(open('./data/ott_service.csv', encoding='UTF-8'))
-ott_header = next(ott_data)
+    # movies_country table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS movies_country (movie_id INT, country_id INT, last_update DATE, PRIMARY KEY (movie_id, country_id))")
 
-myCursor.execute("CREATE TABLE ott_service(movie_id INT PRIMARY KEY, ott_service VARCHAR(255), FOREIGN KEY (movie_id) REFERENCES movies(movie_id))")
-for data in ott_data:
-    data = data[1:]
-    myCursor.execute("INSERT INTO ott_service (movie_id, ott_service) VALUES (%s, %s)", data)
-mydb.commit()
+    with open('./for_use/movies_genre.csv', encoding='UTF-8') as csvfile:
+        movies_country_data = csv.reader(csvfile)
+        header = next(movies_country_data)  # Skip header
+        for data in movies_country_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO movies_country (movie_id, country_id, last_update) VALUES (%s, %s, %s)", (int(data[1]), int(data[2]), last_update))
+    mydb.commit()
+    
+    
+    # country table
+    myCursor.execute("CREATE TABLE IF NOT EXISTS country (country_id INT PRIMARY KEY, country_name VARCHAR(255), last_update DATE)")
 
-
-# genre table
-genre_data = csv.reader(open('./data/genre.csv', encoding='UTF-8'))
-genre_header = next(genre_data)
-
-myCursor.execute("CREATE TABLE genre(id INT PRIMARY KEY, movie_id INT, genre VARCHAR(255), FOREIGN KEY (movie_id) REFERENCES movies(movie_id))")
-for data in genre_data:
-    #data = data[1:]
-    myCursor.execute("INSERT INTO genre (id, movie_id, genre) VALUES (%s, %s, %s)", data)
-mydb.commit()
-
-
-# country table
-country_data = csv.reader(open('./data/country.csv', encoding='UTF-8'))
-country_header = next(country_data)
-
-myCursor.execute("CREATE TABLE country(id INT PRIMARY KEY,movie_id INT, country VARCHAR(255), FOREIGN KEY (movie_id) REFERENCES movies(movie_id))")
-for data in country_data:
-    #data = data[1:]
-    myCursor.execute("INSERT INTO country(id, movie_id, country) VALUES (%s, %s, %s)", data)
-mydb.commit()
-
-
-# actor table
-actor_data = csv.reader(open('./data/cast.csv', encoding='UTF-8'))
-actor_header = next(actor_data)
-
-myCursor.execute("CREATE TABLE actor(id INT PRIMARY KEY,movie_id INT, actor VARCHAR(255), FOREIGN KEY (movie_id) REFERENCES movies(movie_id))")
-for data in actor_data:
-    #data = data[1:]
-    myCursor.execute("INSERT INTO actor(id, movie_id, actor) VALUES (%s, %s, %s)", data)
-mydb.commit()
-
+    with open('./for_use/country.csv', encoding='UTF-8') as csvfile:
+        country_data = csv.reader(csvfile)
+        header = next(country_data)  # Skip header
+        for data in country_data:
+            last_update = datetime.strptime(data[3], '%Y%m%d').date()
+            myCursor.execute("INSERT INTO country (country_id, country_name, last_update) VALUES (%s, %s, %s)", (int(data[1]), data[2], last_update))
+    mydb.commit()
